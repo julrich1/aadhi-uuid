1007e09acc7a8083a9a8ce2467737c926df742de
Jacob Ulrich <jacob.ulrich@nordstrom.com>
2018-06-22 12:28:56 -0700

    reworking device ip

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -81,7 +81,8 @@
 		  		logger.fatal "Invalid scenario: #{params[:scenario_name]} \n"
 		    	render :json => { :status => '404', :message => 'Not Found'}, :status => 404
 		  else
-			    @device = Device.find_or_initialize_by(:device_uuid=>params[:device_uuid])
+				@device = Device.find_or_initialize_by(:device_uuid=>params[:device_uuid])
+				logger.fatal "We did get here"
 			    if params[:isReportRequired] == 'yes'
 			    	@device.update(scenario: @scenario, :isReportRequired=>params[:isReportRequired])
 			    	@device_report = DeviceReport.find_or_initialize_by(:device_uuid=>params[:device_uuid]) 
